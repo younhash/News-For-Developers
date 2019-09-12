33d1aac3234e42045e7e0eece3b37390d0444b76
Mohammad Jaiteh <jaitehmohammad.com>
2019-09-12 10:37:25 -0400

    top 10 news articles are shown

--- script.js
+++ script.js
@@ -23,34 +23,95 @@ const sendMe = async () => {
     // fillTopNews.topArticle[5].content = article.content;
     console.log(fillTopNews.topArticle[0].author)
     
-    topNews.innerHTML = `
+    topNews.innerHTML = ` <div class="article">
     <p> ${fillTopNews.topArticle[0].author}</p>
     <p> ${fillTopNews.topArticle[0].title}</p>
     <p> ${fillTopNews.topArticle[0].description}</p>
     <p> ${fillTopNews.topArticle[0].url}</p>
     <img src= ${fillTopNews.topArticle[0].urlToImage} width="300">
     <p> ${fillTopNews.topArticle[0].publishDate}</p>
-    <p> ${fillTopNews.topArticle[0].content}</p>
+    <p> ${fillTopNews.topArticle[0].content}</p> </div>
 
-     
+    <div class="article"> 
     <p> ${fillTopNews.topArticle[1].author}</p>
     <p> ${fillTopNews.topArticle[1].title}</p>
     <p> ${fillTopNews.topArticle[1].description}</p>
     <p> ${fillTopNews.topArticle[1].url}</p>
     <img src= ${fillTopNews.topArticle[1].urlToImage} width="300">
     <p> ${fillTopNews.topArticle[1].publishDate}</p>
-    <p> ${fillTopNews.topArticle[1].content}</p>
+    <p> ${fillTopNews.topArticle[1].content}</p></div>
     
-    
+    <div class="article">
     <p> ${fillTopNews.topArticle[2].author}</p>
     <p> ${fillTopNews.topArticle[2].title}</p>
     <p> ${fillTopNews.topArticle[2].description}</p>
     <p> ${fillTopNews.topArticle[2].url}</p>
     <img src= ${fillTopNews.topArticle[2].urlToImage} width="300">
     <p> ${fillTopNews.topArticle[2].publishDate}</p>
-    <p> ${fillTopNews.topArticle[2].content}</p> `
+    <p> ${fillTopNews.topArticle[2].content}</p></div> 
+    
+    <div class="article">
+    <p> ${fillTopNews.topArticle[3].author}</p>
+    <p> ${fillTopNews.topArticle[3].title}</p>
+    <p> ${fillTopNews.topArticle[3].description}</p>
+    <p> ${fillTopNews.topArticle[3].url}</p>
+    <img src= ${fillTopNews.topArticle[3].urlToImage} width="300">
+    <p> ${fillTopNews.topArticle[3].publishDate}</p>
+    <p> ${fillTopNews.topArticle[3].content}</p></div>
 
+    <div class="article">
+    <p> ${fillTopNews.topArticle[4].author}</p>
+    <p> ${fillTopNews.topArticle[4].title}</p>
+    <p> ${fillTopNews.topArticle[4].description}</p>
+    <p> ${fillTopNews.topArticle[4].url}</p>
+    <img src= ${fillTopNews.topArticle[4].urlToImage} width="300">
+    <p> ${fillTopNews.topArticle[4].publishDate}</p>
+    <p> ${fillTopNews.topArticle[4].content}</p></div>
 
+    <div class="article">
+    <p> ${fillTopNews.topArticle[5].author}</p>
+    <p> ${fillTopNews.topArticle[5].title}</p>
+    <p> ${fillTopNews.topArticle[5].description}</p>
+    <p> ${fillTopNews.topArticle[5].url}</p>
+    <img src= ${fillTopNews.topArticle[5].urlToImage} width="300">
+    <p> ${fillTopNews.topArticle[5].publishDate}</p>
+    <p> ${fillTopNews.topArticle[5].content}</p></div>
+
+    <div class="article">
+    <p> ${fillTopNews.topArticle[6].author}</p>
+    <p> ${fillTopNews.topArticle[6].title}</p>
+    <p> ${fillTopNews.topArticle[6].description}</p>
+    <p> ${fillTopNews.topArticle[6].url}</p>
+    <img src= ${fillTopNews.topArticle[6].urlToImage} width="300">
+    <p> ${fillTopNews.topArticle[6].publishDate}</p>
+    <p> ${fillTopNews.topArticle[6].content}</p></div>
+
+    <div class="article">
+    <p> ${fillTopNews.topArticle[7].author}</p>
+    <p> ${fillTopNews.topArticle[7].title}</p>
+    <p> ${fillTopNews.topArticle[7].description}</p>
+    <p> ${fillTopNews.topArticle[7].url}</p>
+    <img src= ${fillTopNews.topArticle[7].urlToImage} width="300">
+    <p> ${fillTopNews.topArticle[7].publishDate}</p>
+    <p> ${fillTopNews.topArticle[7].content}</p></div>
+
+    <div class="article">
+    <p> ${fillTopNews.topArticle[8].author}</p>
+    <p> ${fillTopNews.topArticle[8].title}</p>
+    <p> ${fillTopNews.topArticle[8].description}</p>
+    <p> ${fillTopNews.topArticle[8].url}</p>
+    <img src= ${fillTopNews.topArticle[8].urlToImage} width="300">
+    <p> ${fillTopNews.topArticle[8].publishDate}</p>
+    <p> ${fillTopNews.topArticle[8].content}</p></div>
+
+    <div class="article">
+    <p> ${fillTopNews.topArticle[9].author}</p>
+    <p> ${fillTopNews.topArticle[9].title}</p>
+    <p> ${fillTopNews.topArticle[9].description}</p>
+    <p> ${fillTopNews.topArticle[9].url}</p>
+    <img src= ${fillTopNews.topArticle[9].urlToImage} width="300">
+    <p> ${fillTopNews.topArticle[9].publishDate}</p>
+    <p> ${fillTopNews.topArticle[9].content}</p></div> `
 }
 sendMe();
 
